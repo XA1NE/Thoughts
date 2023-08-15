2b0e4d7b7d263bb3f5b5dd27d74c7c9a232b11bf
XA1NE <kamil.kawka@icloud.com>
2023-08-15 18:47:45 +0100

    first commit

---
+++ app/(root)/create-thought/page.tsx
@@ -0,0 +1,24 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import PostThought from "@/components/forms/PostThought";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  // fetch organization list created by user
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  return (
+    <>
+      <h1 className='head-text'>Create a Thought</h1>
+
+      <PostThought userId={userInfo._id} />
+    </>
+  );
+}
+
+export default Page;
